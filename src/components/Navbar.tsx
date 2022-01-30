7d9661e05432a4bb765821b377649614f070b42c
Maksym Menzheha <masimfyd@gmail.com>
2022-01-30 20:17:12 +0200

    Cache Reset

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import NextLink from "next/link";
 import { useMeQuery, useLogoutMutation } from '../generated/graphql';
 import { isServer } from '../utils/isServer';
+import { useRouter } from 'next/router';
 
 interface NavbarProps { }
 
 
 export const NavBar: React.FC<NavbarProps> = ({ }) => {
+    const router = useRouter();
     const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
     const [{ data, fetching }] = useMeQuery({
         pause: isServer(),
@@ -38,7 +40,10 @@ export const NavBar: React.FC<NavbarProps> = ({ }) => {
                 </NextLink>
                 <Box mr={2}>{data.me.username}</Box>
                 <Button
-                    onClick={() => { logout(); }}
+                    onClick={async () => {
+                        await logout();
+                        router.reload();
+                    }}
                     isLoading={logoutFetching}
                     variant='link'
                 >
